4012733077efba579ec6ebfd2f2bdee8fc53065e
faith adline <adlinefaith@gmail.com>
2023-09-18 21:14:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const currentUser = 'Grace Hopper';
+// const welcomeMessage = 'Welcome to Flatbook, ';
+// const welcomeMessage = currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + 'welcome!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, ' + currentUser.slice(0, currentUser.indexOf(' ')) + '!';
+
+
 // Write your code in this file!
